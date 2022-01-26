a8ddb2eadd3e88ea1cee995d52d79278249a21ba
Santiago <Martnez Bota>
2022-01-26 16:30:06 +0100

    migración de la tabla 'superheroe' y 'login'

---
+++ superheroes-crud-auth-authorize/database/migrations/2022_01_26_150752_create_superheroe_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuperheroeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('superheroe', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->integer('edad');
+            $table->date('fecha_nacimiento');
+            $table->boolean('poderes');
+            $table->enum('genero', ['hombre', 'mujer']);
+            $table->string('descripcion');
+            $table->string('vengador');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('superheroe');
+    }
+}
