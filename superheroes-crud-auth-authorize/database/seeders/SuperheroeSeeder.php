7118d7bc71d311e126f475795730c2f04e45d55c
Santiago <Martnez Bota>
2022-01-26 17:44:33 +0100

    seeder creado con dos registros de manera predeterminada

---
+++ superheroes-crud-auth-authorize/database/seeders/SuperheroeSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class SuperheroeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //DB::table('personaje')->insert([
+        //            'nombre'=> 'Spiderman',
+        //            'edad' => 23,
+        //            'nacionalidad'=> 'USA',
+        //            "created_at" =>  date('Y-m-d H:i:s'),
+        //            "updated_at" => date('Y-m-d H:i:s')
+        //        ]);
+
+        DB::table('superheroe')->insert([
+            'nombre'=> 'Spiderman',
+            'edad'=> 23,
+            'fecha_nacimiento' => Carbon::parse('1999-01-01'),
+            'poderes'=> 'SUPERFUERZA',
+            'genero' => 'hombre',
+            'descripcion' => 'El hombre araña',
+            'vengador' => "SI"
+        ]);
+
+        DB::table('superheroe')->insert([
+            'nombre'=> 'Thor',
+            'edad'=> 500,
+            'fecha_nacimiento' => Carbon::parse('1400-01-01'),
+            'poderes'=> 'SUPERFUERZA',
+            'genero' => 'hombre',
+            'descripcion' => 'Dios del trueno',
+            'vengador' => "SI"
+        ]);
+    }
+}
