8d8ca63644567dc2e45cb85701cfedd2c0825fa6
Santiago <Martnez Bota>
2022-01-30 18:14:22 +0100

    old values añadidos a los distintos campos de los formularios de create y edit

--- superheroes-crud-auth-authorize/resources/views/superheroe/create.blade.php
+++ superheroes-crud-auth-authorize/resources/views/superheroe/create.blade.php
@@ -12,28 +12,32 @@
             @method('POST')
             <div class="mb-3">
                 <label class="form-label">@lang('NOMBRE')
-                    <input class="form-control" type="text" id="nombre" name="nombre"/>
+                    <input class="form-control" type="text" id="nombre" name="nombre" value="{{ old('nombre') }}"/>
                 </label>
             </div>
             <div class="mb-3">
                 <label class="form-label">@lang('EDAD')
-                    <input class="form-control" type="number" id="edad" name="edad"/>
+                    <input class="form-control" type="number" id="edad" name="edad" value="{{ old('edad') }}"/>
                 </label>
             </div>
 
             <div class="mb-3">
                 <label class="form-label">@lang('FECHA DE NACIMIENTO')
-                    <input class="form-control" type="date" id="fecha_nacimiento" name="fecha_nacimiento"/>
+                    <input class="form-control" type="date" id="fecha_nacimiento" name="fecha_nacimiento"
+                           value="{{ old('fecha_nacimiento') }}"/>
                 </label>
             </div>
 
             <div class="mb-3">
                 <label class="form-label">@lang('PODERES')
-                    <input class="form-check-input" type="checkbox" id="poderes1" name="poderes[]" value="volar"/>
+                    <input class="form-check-input" type="checkbox" id="poderes1" name="poderes[]" value="volar"
+                        {{ (is_array(old('poderes')) and in_array('volar', old('poderes'))) ? 'checked' : '' }}/>
                     <label for="poderes1" class="form-check-label">@lang('VOLAR')</label>
-                    <input class="form-check-input" type="checkbox" id="poderes2" name="poderes[]" value="rapidez"/>
+                    <input class="form-check-input" type="checkbox" id="poderes2" name="poderes[]" value="rapidez"
+                        {{ (is_array(old('poderes')) and in_array('rapidez', old('poderes'))) ? 'checked' : '' }}/>
                     <label for="poderes2" class="form-check-label">@lang('RAPIDEZ')</label>
-                    <input class="form-check-input" type="checkbox" id="poderes3" name="poderes[]" value="superfuerza"/>
+                    <input class="form-check-input" type="checkbox" id="poderes3" name="poderes[]" value="superfuerza"
+                        {{ (is_array(old('poderes')) and in_array('superfuerza', old('poderes'))) ? 'checked' : '' }}/>
                     <label for="poderes3" class="form-check-label">@lang('SUPERFUERZA')</label>
                 </label>
             </div>
@@ -42,23 +46,25 @@
                 <div class="mb-3">@lang('GÉNERO')</div>
                 <div class="mb-3">
                     <label for="genero1" class="form-check-label">@lang('HOMBRE')</label>
-                    <input type="radio" id="genero1" name="genero" value="hombre"/>
+                    <input type="radio" id="genero1" name="genero" value="hombre"
+                    {{ old('genero') == 'hombre' ? 'checked': '' }}/>
                     <label for="genero2" class="form-check-label">@lang('MUJER')</label>
-                    <input type="radio" id="genero2" name="genero" value="mujer"/>
+                    <input type="radio" id="genero2" name="genero" value="mujer"
+                        {{ old('genero') == 'mujer' ? 'checked': '' }}/>
                 </div>
             </div>
 
             <div class="mb-3">
                 <label class="form-label">@lang('DESCRIPCIÓN')
-                    <textarea class="form-control" id="descripcion" name="descripcion"></textarea>
+                    <textarea class="form-control" id="descripcion" name="descripcion">{{ old('descripcion') }}</textarea>
                 </label>
             </div>
 
             <div class="mb-3">
                 <label class="form-label">@lang('VENGADOR')
                     <select name="vengador" id="vengador">
-                        <option value="si">@lang('SÍ')</option>
-                        <option value="no">@lang('NO')</option>
+                        <option value="si" {{ old('vengador') == 'si' ? 'selected': '' }}>@lang('SÍ')</option>
+                        <option value="no" {{ old('vengador') == 'no' ? 'selected': '' }}>@lang('NO')</option>
                     </select>
                 </label>
             </div>
