0a12d45a4b37bd7dd86137d801c64fbd16e12bc3
Santiago <Martnez Bota>
2022-02-10 17:59:44 +0100

    migracion posts hecha

---
+++ smartinez-examen10/database/migrations/2022_02_10_165445_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('extracto');
+            $table->string('contenido');
+            $table->boolean('caducable');
+            $table->boolean('comentable');
+            $table->enum('acceso',['privado','publico']);
+            $table->date('fecha_publicacion');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
